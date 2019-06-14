dd81effed3e91267751533a9aa72fe8a810c1bf4
Erik Remington Lee <eriklee19@gmail.com>
2019-06-14 17:23:50 -0400

    working with serveR

--- python-server/python-server.py
+++ python-server/python-server.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
-
+import requests
 app = Flask(__name__)
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def home_page(path):
     return render_template('index.html')
+
+@app.route('/stocks', methods=['GET', 'POST'])
+def stock_info():
+    r=requests.get('https://cloud.iexapis.com/v1/stock/TSLA/quote?token=pk_de4620b808c14be59ad8257623d8a6d2')
+    print(r.status_code)
+    return r.json()
+
+
 
 
 if __name__ =='__main__':
