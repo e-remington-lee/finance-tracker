a86e764d330e8890a32a6a12cc87ac5d9b797cd3
Erik Remington Lee <eriklee19@gmail.com>
2019-06-24 16:26:24 -0400

    uses hard coded json data to display line chart dates

--- angular/src/app/buy-sell/buy-sell.component.ts
+++ angular/src/app/buy-sell/buy-sell.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StocksService } from '../stocks.service';
 import { Chart } from 'chart.js';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-buy-sell',
@@ -56,44 +57,50 @@ export class BuySellComponent implements OnInit {
       this.searchStockData = data;
       console.log(this.searchStockSymbol);
     });
-    
+
     var ctx = document.getElementById('myChart');
     new Chart(ctx, {
-      type: 'bar',
+      type: 'line',
       data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-              label: '# of Votes',
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255, 99, 132, 1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
-              borderWidth: 1
-          }]
+        labels: ['2019-6-22', '2019-6-23', '2019-6-24', '2019-6-25', '2019-6-26',
+         '2019-6-27', '2019-6-28', '2019-6-29', '2019-6-30', '2019-7-1',
+          '2019-6-27', '2019-6-28', '2019-6-29', '2019-6-30', '2019-7-1',
+          '2019-6-22', '2019-6-23', '2019-6-24', '2019-6-25', '2019-6-26', '2019-6-27', '2019-6-28', '2019-6-29', '2019-6-30', '2019-7-1', '2019-6-27', '2019-6-28', '2019-6-29', '2019-6-30', '2019-7-1'],
+        datasets: [{ 
+            data: [86,114,106,106,107,111,133,221,783,2478],
+            label: "Africa",
+            borderColor: "#3e95cd",
+            fill: false
+          }, { 
+            data: [282,350,411,502,635,809,947,1402,3700,5267],
+            label: "Asia",
+            borderColor: "#8e5ea2",
+            fill: false
+          }, { 
+            data: [168,170,178,190,203,276,408,547,675,734],
+            label: "Europe",
+            borderColor: "#3cba9f",
+            fill: false
+          }, { 
+            data: [40,20,10,16,24,38,74,167,508,784],
+            label: "Latin America",
+            borderColor: "#e8c3b9",
+            fill: false
+          }, { 
+            data: [6,3,2,2,7,26,82,172,312,433],
+            label: "North America",
+            borderColor: "#c45850",
+            fill: false
+          }
+        ]
       },
       options: {
-          scales: {
-              yAxes: [{
-                  ticks: {
-                      beginAtZero: true
-                  }
-              }]
-          }
+        title: {
+          display: true,
+          text: `Previous Month's Stock Prices for ${this.searchStockSymbol}`
+        }
       }
-  });
+    });
   }
 
   buyStockButton2(symbol) {
