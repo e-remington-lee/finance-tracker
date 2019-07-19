2d24752ebf48f8a0d29881d7cb53431ae6a9757f
Erik Remington Lee <eriklee19@gmail.com>
2019-07-19 12:22:53 -0400

    adding unittests to routes

--- python-server/test_server.py
+++ python-server/test_server.py
@@ -1,7 +1,22 @@
 import unittest
 import server
 import stock_calculator
+import decimal
 
 # print(stock_calculator.calculate_price(1,2))
 
-# class test_calculator(unittest.TestCase):
+class Test_calculator(unittest.TestCase):
+
+    def test_add(self):
+        result = stock_calculator.calculate_price(5.50, 10)
+        self.assertIsInstance(type(result), type(float))
+    
+    # def test_decimals(self):
+        #check to return a decimal?
+
+class Test_endpoints(unittest.TestCase):
+    def test_login(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
