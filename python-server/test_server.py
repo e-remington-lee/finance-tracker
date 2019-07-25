b86e19e628062c7dc8bdddc8be242613246347f7
Erik Remington Lee <eriklee19@gmail.com>
2019-07-24 20:19:10 -0400

    more tests

--- python-server/test_server.py
+++ python-server/test_server.py
@@ -9,6 +9,7 @@ from unittest.mock import Mock, patch
 class Test_calculator(unittest.TestCase):
     def test_add(self):
         result = stock_calculator.calculate_price(5.50, 10)
+
         self.assertIsInstance(type(result), type(float))
     
     # def test_decimals(self):
@@ -25,10 +26,12 @@ class Test_login(unittest.TestCase):
         mock_request.headers = { 'x-email': 'fakeemail3@gmail.com',
                                 'x-password': '1234'}
         mock_login_account.return_value = {'user_id': 1, 'account_id': 1, 'first_name': 'Remy'}
+        
         server.login_user()
         mock_jsonify.assert_called_once()
 
         mock_login_account.return_value = None
+
         server.login_user()
         mock_make_response.assert_called_once()
 
@@ -46,6 +49,7 @@ class Test_registration(unittest.TestCase):
         'email': 'fake@gmail.com',
         'password': 'password'
         }
+        
         server.register_user()
         mock_make_response.assert_called_once()
 
@@ -55,10 +59,6 @@ class Test_registration(unittest.TestCase):
 
 
 class Test_stock_info(unittest.TestCase):
-    @classmethod
-    def setupVariables(cls):
-        cls.change_percent = 0.2
-    
     @patch("server.money")
     @patch("server.jsonify")
     @patch("server.iex_stock_data")
@@ -66,22 +66,115 @@ class Test_stock_info(unittest.TestCase):
     def test_stock_info(self, mock_request, mock_iex, mock_jsonify, mock_money):
         mock_request.args.get.return_value = 'BYND'
         mock_iex.return_value = None
+
         server.stock_info()
         mock_jsonify.assert_not_called()
 
-
         mock_iex.return_value = mock_request.json
         mock_request.json.return_value = {'key': 'value'}
-        # stock_data = [{
-        #                 'latestPrice': 200,
-        #                 'companyName': 'Amazon',
-        #                 'symbol': 'BYND',
-        #                 'changePercent': 0.02,
-        #                 'change': 6.00
-        #                 }]
 
         server.stock_info()
         mock_jsonify.assert_called_once()
+
+
+class Test_historical_info(unittest.TestCase):
+    @patch("server.money")
+    @patch("server.jsonify")
+    @patch("server.iex_stock_data")
+    @patch("server.request")
+    def test_historical_info(self, mock_request, mock_iex, mock_jsonify, mock_money):
+        mock_request.args.get.return_value = 'BYND'
+        mock_iex.return_value = mock_request.json
+        mock_request.json.return_value = {'key': 'value'}
+
+        server.historical_info()
+        mock_jsonify.assert_called_once()
+
+
+class Test_balance_change_buy(unittest.TestCase):
+    @patch('server.update_balance_buy')
+    @patch('server.calculate_price')
+    @patch("server.jsonify")
+    @patch("server.iex_latest_price")
+    @patch("server.request")
+    def test_balance_change_buy(self, mock_request, mock_iex, mock_jsonify, mock_calculate, mock_balance_buy):
+        mock_request.get_json.return_value = [{
+                                            'symbol': 'value',
+                                            'accountId': 1,
+                                            'shares': 5
+                                            }]  
+        
+        mock_iex.return_value = 210.00
+        mock_calculate.return_value = 21.0
+        mock_balance_buy.return_value = 21.0
+
+        server.balance_change_buy()
+        mock_jsonify.assert_called_once()
+
+
+class Test_balance_change_sell(unittest.TestCase):
+    @patch('server.update_balance_sell')
+    @patch('server.calculate_price')
+    @patch("server.jsonify")
+    @patch("server.iex_latest_price")
+    @patch("server.request")
+    def test_balance_change_sell(self, mock_request, mock_iex, mock_jsonify, mock_calculate, mock_balance_sell):
+        mock_request.get_json.return_value = [{
+                                            'symbol': 'value',
+                                            'accountId': 1,
+                                            'shares': 5
+                                            }]  
+        
+        mock_iex.return_value = 210.00
+        mock_calculate.return_value = 21.0
+        mock_balance_sell.return_value = 21.0
+
+        server.balance_change_sell()
+        mock_jsonify.assert_called_once()
+
+
+class Test_transactions(unittest.TestCase):
+    @patch("server.transaction_list")
+    @patch("server.jsonify")
+    @patch("server.iex_latest_price")
+    @patch("server.request")
+    def test_transactions(self, mock_request, mock_iex, mock_jsonify, mock_transaction):
+        mock_request.get_json.return_value = [{
+                                            'symbol': 'value',
+                                            'accountId': 1,
+                                            'shares': 5,
+                                            'type': 'buy'
+                                            }]  
+        
+        mock_iex.return_value = 210.00
+        mock_transaction.return_value = 'Test'
+
+        server.transactions()
+        mock_jsonify.assert_called_once()
+
+
+class Test_check_balance(unittest.TestCase):
+    @patch("server.check_account_balance")
+    @patch("server.calculate_price")
+    @patch("server.money")
+    @patch("server.jsonify")
+    @patch("server.iex_latest_price")
+    @patch("server.request")
+    def test_check_balance(self, mock_request, mock_iex, mock_jsonify, mock_money, mock_calculate, mock_account_balance):
+        mock_request.args.get.return_value = '10'
+        mock_iex.return_value = 21
+        mock_calculate.return_value = 210
+        mock_account_balance.return_value = {'account_balance': 1}
+        result = server.check_balance()
+        self.assertEqual(result, ("", 404))
+
+        mock_calculate.return_value = 210
+        mock_account_balance.return_value = {'account_balance': 300}
+        result = server.check_balance()
+        self.assertEqual(result, ("", 200))
+
+
+
 
 
 if __name__ == '__main__':
